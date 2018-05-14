f3ef43bbae46933fda3df80946a71e4fac1f4666
klrqn <burtonsb@gmail.com>
2018-05-14 19:27:50 +0000

    jquery selectors #6

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,22 @@
-$(document).ready(function(){
-	alert('jquery linked.');
-})
+// $("header nav li:first").css({border: "2px solid red"});
+// $("header nav li:last").css({border: "2px solid green"});
+
+
+// $("#contact ul:first").css({border: "2px solid blue"});
+// $("#contact ul:last").css({border: "2px solid purple"});
+
+// $("header nav li:even").css({border: "3px dashed green"});
+// $("header nav li:odd").css({border: "3px dashed yellow"});
+
+// $("section:not('#contact')").css({border: "2px dashed black"});
+
+// // greater than less than
+// $("#social-nav li:lt(2)").css({border: "5px solid blue"});
+// $("#social-nav li:gt(2)").css({border: "5px solid pink"});
+
+// can be used to tag elements that have any attribute associated with them
+$("img[alt]").css({border: "2px dashed pink"});
+$("div[class]").css({border: "2px dashed pink"});
+$("img[alt=quote").css({border: "5px solid blue"});
+
+alert("api.jquery.com/category/selectors");
