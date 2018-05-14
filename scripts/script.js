5fb18b983e4c418f54a6b45a2c40178496f2c077
klrqn <burtonsb@gmail.com>
2018-05-14 22:02:13 +0000

    #9 inserting html

--- scripts/script.js
+++ scripts/script.js
@@ -21,7 +21,18 @@
 // // closest moves UP the DOM
 // $("#social-nav").closest(".wrapper").css({border: "3px solid red"});
 
-// all methods chained together
-$("#contact-methods").css({ border: "2px solid red" })
-	.next().css({ border: "2px dashed green" })
-	.closest("section").css({ border: "2px solid blue" });
+// // all methods chained together
+// $("#contact-methods").css({ border: "2px solid red" })
+// 	.next().css({ border: "2px dashed green" })
+// 	.closest("section").css({ border: "2px solid blue" });
+
+var tweet = "<div style='margin: 20px; padding: 10px; background: #ddd'>The big fight live: Ham vs Cheese!</div>"
+
+// $("#tweets div").append(tweet);
+// $("#tweets div").prepend(tweet);
+
+// $("#tweets div p").before(tweet);
+// $("#tweets div p").after(tweet);
+
+// $("#tweets div").html(tweet);
+$("#tweets div p").text(tweet);
