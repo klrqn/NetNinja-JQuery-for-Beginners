85b17e92398ed74b42288973e9f30c6743ceb844
klrqn <burtonsb@gmail.com>
2018-05-22 23:21:11 +0000

    document on ready and window load

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-// #16
+// #17
 
 $("#lead-banner").click(function() {
 	alert('you clicked the lead banner id')
@@ -7,3 +7,22 @@ $("#lead-banner").click(function() {
 
 // list of jQuery Event Methods
 // https://www.w3schools.com/jquery/jquery_ref_events.asp
+
+$(document).on("ready", function() {
+	alert('on ready!');
+})
+
+//shorthand jquery! this particular event shorthand
+$(function(){
+	alert('everything goes in here');
+});
+
+
+$(window).on("load", function(){
+	console.log('this waits for the window to load to execute');
+});
+
+//shorthand for above
+$(window).load(function(){
+	console.log('see above');
+})
